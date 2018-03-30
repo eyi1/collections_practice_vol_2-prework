41a469e8a88a9685f6a80c791f0863112be39ad0
eyi1 <ej7729@gmail.com>
2018-03-30 04:36:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,14 +1,19 @@
 # your code goes here
-def begins_with_r (arr)
-  arr.all? do |value|
-  if value[0] == "r"
-    return true
-  else
-    return false
+# def begins_with_r (arr)
+#   arr.all? do |value, index|
+#   if value[0] == "r"
+#     return true
+#   else
+#     return false
 
+def begins_with_r (arr)
+  arr.all? do |string|
+    string[0] == "r"
+  end
+end
 
 
     #value.start_with?("r") ? true : false
-  end
-end
-end
+#   end
+# end
+# end
